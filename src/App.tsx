2507a044fb208b80e45459d8c0b01123bdc67ec8
Hiro-mackay <johnmackay150@gmail.com>
2020-11-08 11:28:10 +0900

    FIX add rectangle object & move event

--- src/App.tsx
+++ src/App.tsx
@@ -1,78 +1,105 @@
 import React from "react";
 import * as PIXI from "pixi.js";
 
-abstract class ModelBase<TDisplayObject extends PIXI.DisplayObject> {
-  public constructor(
-    public readonly displayObject: TDisplayObject,
-    x: number,
-    y: number
-  ) {
-    this.displayObject.position.set(x, y);
-  }
+// abstract class ModelBase<TDisplayObject extends PIXI.DisplayObject> {
+//   public constructor(
+//     public readonly displayObject: TDisplayObject,
+//     x: number,
+//     y: number
+//   ) {
+//     this.displayObject.position.set(x, y);
+//   }
 
-  public update(x: number, y: number): void {
-    this.doUpdate(x, y);
-  }
+//   public update(x: number, y: number): void {
+//     this.doUpdate(x, y);
+//   }
 
-  protected abstract doUpdate(x: number, y: number): void;
-}
+//   protected abstract doUpdate(x: number, y: number): void;
+// }
 
-class Rectangle extends ModelBase<PIXI.Graphics> {
-  constructor() {
-    super(new PIXI.Graphics(), 0, 0);
-    this.displayObject.beginFill(0xde3249);
-    this.displayObject.drawRect(50, 50, 100, 100);
-    this.displayObject.endFill();
+// class Rectangle extends ModelBase<PIXI.Graphics> {
+//   constructor() {
+//     super(new PIXI.Graphics(), 0, 0);
+//     this.displayObject.beginFill(0xde3249);
+//     this.displayObject.drawRect(50, 50, 100, 100);
+//     this.displayObject.endFill();
 
-    this.displayObject.interactive = true;
-    this.displayObject.buttonMode = true;
+//     this.displayObject.interactive = true;
+//     this.displayObject.buttonMode = true;
 
-    this.displayObject
-      .on("mousedown", (e: PIXI.InteractionEvent, _t = this) =>
-        this.onDragStart(e, _t)
-      )
-      .on("mouseup", (e: PIXI.InteractionEvent, _t = this) =>
-        this.onDragEnd(e, _t)
-      )
-      .on("mouseupoutside", (e: PIXI.InteractionEvent, _t = this) =>
-        this.onDragEnd(e, _t)
-      )
-      .on("mousemove", (e: PIXI.InteractionEvent, _t = this) =>
-        this.onDragMove(e, _t)
-      );
-  }
+//     this.displayObject
+//       .on("pointerdown", (e: PIXI.InteractionEvent, _t = this) =>
+//         this.onDragStart(e, _t)
+//       )
+//       .on("pointerdown", (e: PIXI.InteractionEvent, _t = this) =>
+//         this.onDragEnd(e, _t)
+//       )
+//       .on("pointerdown", (e: PIXI.InteractionEvent, _t = this) =>
+//         this.onDragEnd(e, _t)
+//       )
+//       .on("pointermove", (e: PIXI.InteractionEvent, _t = this) =>
+//         this.onDragMove(e, _t)
+//       );
+//   }
 
-  protected doUpdate(x: number, y: number) {
-    this.displayObject.x = x;
-    this.displayObject.y = y;
-  }
+//   protected doUpdate(x: number, y: number) {
+//     this.displayObject.x = x;
+//     this.displayObject.y = y;
+//   }
 
-  private onDragStart(e: PIXI.InteractionEvent, t: this) {
-    t.displayObject.alpha = 0.5;
-    console.log("onDragStart");
-  }
-  private onDragEnd(e: PIXI.InteractionEvent, t: this) {
-    t.displayObject.alpha = 1;
-  }
-  private onDragMove(e: PIXI.InteractionEvent, t: this) {
-    const mousePosition = e.data.getLocalPosition(e.currentTarget.parent);
-    t.update(
-      mousePosition.x - t.displayObject.width,
-      mousePosition.y - t.displayObject.height
-    );
-  }
-}
-
-const t = new Rectangle();
+//   private onDragStart(e: PIXI.InteractionEvent, t: this) {
+//     t.displayObject.alpha = 0.5;
+//     console.log("onDragStart");
+//   }
+//   private onDragEnd(e: PIXI.InteractionEvent, t: this) {
+//     t.displayObject.alpha = 1;
+//   }
+//   private onDragMove(e: PIXI.InteractionEvent, t: this) {
+//     const mousePosition = e.data.getLocalPosition(e.currentTarget.parent);
+//     console.log("object", mousePosition);
+//     t.update(
+//       mousePosition.x - t.displayObject.width,
+//       mousePosition.y - t.displayObject.height
+//     );
+//   }
+// }
 
 function App() {
   const [pixiApp, setPixiApp] = React.useState<PIXI.Application | null>(null);
   const canvasRef = React.useRef(null);
 
+  function onDragStart(
+    this: PIXI.Graphics,
+    event: PIXI.InteractionEvent
+  ): void {
+    this.on("pointermove", onDragMove);
+  }
+  function onDragEnd(this: PIXI.Graphics, event: PIXI.InteractionEvent): void {
+    this.off("pointermove");
+  }
+  function onDragMove(this: PIXI.Graphics, event: PIXI.InteractionEvent): void {
+    const newPosition = event.data.getLocalPosition(this.parent);
+    this.x = newPosition.x - this.width;
+    this.y = newPosition.y - this.height;
+  }
+
   function drawRectangle() {
     if (!pixiApp) return;
 
-    pixiApp.stage.addChild(t.displayObject);
+    const g = new PIXI.Graphics();
+
+    g.beginFill(0xde3249);
+    g.drawRect(50, 50, 100, 100);
+    g.endFill();
+
+    g.interactive = true;
+    g.buttonMode = true;
+
+    g.on("pointerdown", onDragStart)
+      .on("pointerup", onDragEnd)
+      .on("pointerupoutside", onDragEnd);
+
+    pixiApp.stage.addChild(g);
   }
 
   React.useEffect(() => {
